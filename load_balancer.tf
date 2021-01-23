0b4d0d6fcbfc07d9b9f070a7b41219b918536120
Pedro Guerra <5890852+PedroGuerraPT@users.noreply.github.com>
2021-01-23 23:50:38 +0000

    Added HTTPS listener to ELB

--- load_balancer.tf
+++ load_balancer.tf
@@ -9,13 +9,13 @@ resource "aws_elb" "beeper-elb" {
     lb_protocol       = "http"
   }
 
-  /* listener {
+  listener {
     instance_port      = 443
     instance_protocol  = "http"
     lb_port            = 443
     lb_protocol        = "https"
     ssl_certificate_id = aws_acm_certificate.cert.id
-  } */
+  }
 
   health_check {
     healthy_threshold   = 2
